b0bcc4741041a42be89f9ac87c1bda806b5bd367
jessicatrevisan <jessica.trevisan.souza@escola.pr.gov.br>
2026-05-29 10:57:05 +0000

    LISTA

--- lista 1/b3.C
+++ lista 1/b3.C
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-// Definindo a estrutura Jogador
 typedef struct {
     char nome[50];
     int pontos;
@@ -13,7 +12,6 @@ int main() {
     printf("Digite a quantidade de jogadores: ");
     scanf("%d", &n);
 
-    // Validação para garantir que N seja válido
     if (n <= 0) {
         printf("Quantidade invalida!\n");
         return 0;
@@ -21,20 +19,17 @@ int main() {
 
     Jogador jogadores[n];
 
-    // Lendo os dados de cada jogador
     for (int i = 0; i < n; i++) {
         printf("\n--- Jogador %d ---\n", i + 1);
         printf("Nome: ");
-        scanf(" %[^\n]s", jogadores[i].nome); // Lê strings com espaços
+        scanf(" %[^\n]s", jogadores[i].nome); 
         printf("Pontos: ");
         scanf("%d", &jogadores[i].pontos);
     }
 
-    // Variáveis para armazenar o indice do maior e do menor
     int indiceMaior = 0;
     int indiceMenor = 0;
 
-    // Lógica para encontrar o jogador com mais e menos pontos
     for (int i = 1; i < n; i++) {
         if (jogadores[i].pontos > jogadores[indiceMaior].pontos) {
             indiceMaior = i;
@@ -44,7 +39,6 @@ int main() {
         }
     }
 
-    // Exibindo os resultados
     printf("\n=============================\n");
     printf("Jogador com mais pontos: %s (%d pontos)\n", jogadores[indiceMaior].nome, jogadores[indiceMaior].pontos);
     printf("Jogador com menos pontos: %s (%d pontos)\n", jogadores[indiceMenor].nome, jogadores[indiceMenor].pontos);
